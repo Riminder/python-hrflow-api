78b9fc816f64d0fa9339df6dd79af94a25aca312
Ala Eddine <alaeddine.chhima@riminder.net>
2020-08-06 13:00:59 +0200

    FIX README

--- hrflow/job/indexing.py
+++ hrflow/job/indexing.py
@@ -14,7 +14,7 @@ class JobIndexing():
         response = self.client.post("job/indexing", json=job_json)
         return response.json()
 
-    def put(self, board_key, key, job_json):
+    def edit(self, board_key, key, job_json):
         job_json['board_key'] = validate_key("Board", board_key)
         job_json['key'] = validate_key("Job", key)
         """Use the api to add a new profile using profile_data."""
