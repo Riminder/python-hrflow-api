16cb4400727b1c12209194588edf04eda986229d
Ala Eddine <alaeddine.chhima@riminder.net>
2020-08-07 16:48:29 +0200

    Update Colabs

--- hrflow/job/__init__.py
+++ hrflow/job/__init__.py
@@ -16,36 +16,3 @@ class Job(object):
         self.searching = JobSearching(self.client)
         self.scoring = JobScoring(self.client)
         self.reasoning = JobReasoning(self.client)
-
-    def add_json(self, name, agent_id, job_reference, job_labels=[], job_metadatas=[], **kwargs):
-        """
-                Use the api to add a new job using json_data.
-
-                Args:
-                    name:               <string>
-                                        name
-                    agent_id:           <string>
-                                        agent_id
-                    job_reference:      <string>
-                                        job_reference
-                    job_labels:         <list>
-                                        job_labels
-                    job_metadatas:      <list>
-                                        job_metadatas
-                    agent_id:           <string>
-                                        agent_id
-                    kwargs:             additional information
-
-                Returns
-                    201 if the job is successfully created
-        """
-        payload = {
-            'name': name,
-            'agent_id': agent_id,
-            "job_reference": job_reference,
-            "job_labels": job_labels,
-            "job_metadatas": job_metadatas
-        }
-        data = {**payload, **kwargs}
-        response = self.client.post("job", data=data)
-        return response.json()
