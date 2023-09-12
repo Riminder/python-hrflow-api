4b173b69a0d709f01461a58fa665b95fdd117c97
Mustapha AJEGHRIR <66799406+Mustapha-AJEGHRIR@users.noreply.github.com>
2023-09-12 14:28:37 +0200

    Adding batch tagging
    Tagging can be done in batches too

--- hrflow/hrflow/text/tagging.py
+++ hrflow/hrflow/text/tagging.py
@@ -13,8 +13,9 @@ class TextTagging:
         Predict most likely tags for a text with our library of AI algorithms.
 
         Args:
-            text:                   <string>
+            text:                   <string or list of strings>
                                     Target text input. Example: the full text of a Job, a Resume , a Profile, an experience, a Job and more
+                                    Or a list of target inpus texts. Example : ["Text 1", "Text 2", ...] 
             algorithm_key:          <string>
                                     AI tagging algorithm you want to apply to the input text.
                                     Five taggers have been released through the Tagging API. We are actively working on bringing out more taggers.
@@ -33,12 +34,22 @@ class TextTagging:
             Predictions tags with probabilities
 
         """
-        payload = {
-            "text": text,
-            "algorithm_key": algorithm_key,
-            "top_n": top_n,
-            "output_lang": output_lang,
-        }
+        if isinstance(text, str):
+            payload = {
+                "text": text,
+                "algorithm_key": algorithm_key,
+                "top_n": top_n,
+                "output_lang": output_lang,
+            }
+        elif isinstance(text, list):
+            payload = {
+                "texts": text,
+                "algorithm_key": algorithm_key,
+                "top_n": top_n,
+                "output_lang": output_lang,
+            }
+        else :
+            raise ValueError("Input text must be a string or a list of strings.")
 
         response = self.client.post("text/tagging", json=payload)
         return validate_response(response)
