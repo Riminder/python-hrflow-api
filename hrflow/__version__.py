e07c2dec3d0ff7999525b9e19dcd6f3de5cffa5f
Corentin Duchêne <corentin.duchene@hrflow.ai>
2023-12-08 16:50:38 +0100

    build: move project to poetry and pytest

--- hrflow/__version__.py
+++ hrflow/__version__.py
@@ -1,7 +1,9 @@
+import importlib.metadata
+
 __title__ = "hrflow"
 __description__ = "Python hrflow.ai API package"
 __url__ = "https://github.com/hrflow/python-hrflow-api"
-__version__ = "3.1.1"
+__version__ = importlib.metadata.version("hrflow")
 __author__ = "HrFlow.ai"
 __author_email__ = "contact@hrflow.ai"
 __license__ = "MIT"
